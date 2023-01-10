d0d88ea946aa5c0d498ca79bcd1b07b923c8332e
daniel.sousa <danieloliveirasousaa@gmail.com>
2023-01-10 15:45:56 -0300

    adicionado o swagger

--- src/main/java/com/conversordemoedas/controllers/ConversorController.java
+++ src/main/java/com/conversordemoedas/controllers/ConversorController.java
@@ -2,21 +2,24 @@ package com.conversordemoedas.controllers;
 
 import com.conversordemoedas.enums.CodeCoin;
 import com.conversordemoedas.services.ConversorService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
 @RestController
+@RequestMapping(value = "/conversor")
+@Api(value = "API REST CONVERSOR")
+@CrossOrigin(origins = "*")// Liberatodos os dominios e acessar a api
 public class ConversorController {
 
     @Autowired
     private ConversorService conversorService;
 
     @GetMapping(path = "/{valor}/{moeda}/{moedaParaConverter}")
+    @ApiOperation(value = "Retorna a conversao da valor da moeda ")
     public String getConverter(@PathVariable("valor") Double valor,
                                @PathVariable("moeda") CodeCoin moeda,
                                @PathVariable("moedaParaConverter") CodeCoin moedaParaConverter) throws IOException, InterruptedException {
